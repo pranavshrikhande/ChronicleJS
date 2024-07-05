d8c894290cb2f3fe8fbb1621ad09b3f408b9d752
Pranav Shrikhande <pranav.shrikhande@gmail.com>
2024-07-05 00:25:35 -0400

    feat: added axios file and moved posts

--- src/App.js
+++ src/App.js
@@ -14,32 +14,7 @@ import { format } from "date-fns";
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "My First Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 2,
-      title: "My 2nd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 3,
-      title: "My 3rd Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    },
-    {
-      id: 4,
-      title: "My Fourth Post",
-      datetime: "July 01, 2021 11:17:36 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
-    }
-  ])  
+  const [posts, setPosts] = useState([])  
 
 
   const[search, setSearch] = useState('');
@@ -66,7 +41,7 @@ function App() {
 
     const id = posts.length ? Number(posts[posts.length-1].id) + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-    const newPost = {id, title: postTitle, body: postBody};
+    const newPost = {id, title: postTitle, body: postBody, datetime: datetime};
     const allPosts = [...posts, newPost];
     setPosts(allPosts);
     setPostTitle('');
