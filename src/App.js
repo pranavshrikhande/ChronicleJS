8ec5e586b388265093f861c9375095c8e2bb56af
Pranav Shrikhande <pranav.shrikhande@gmail.com>
2024-07-05 00:39:28 -0400

    feat: posts feed from live server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //React router routes components
 
-import { Route, Routes ,useNavigate  } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Header from "./Header";
 import Nav from "./Nav";
@@ -11,66 +11,103 @@ import About from "./About";
 import Missing from "./Missing";
 import Footer from "./Footer";
 import { format } from "date-fns";
+import api from "./api/posts";
 
 function App() {
+  const [posts, setPosts] = useState([]);
 
-  const [posts, setPosts] = useState([])  
-
-
-  const[search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
 
   const navigate = useNavigate(); //React Router v6 introduced a paradigm shift with the useNavigate hook, offering a more concise and streamlined approach to manage navigation within components. Unlike useHistory, useNavigate simplifies the navigation process by providing a single function for triggering route changes.
 
+  //another useEffect hook to load data to fetch data and needs to happen at load time, so dependency should be []
 
-  useEffect(()=>{
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get("/posts");
+
+        if (response && response.data) {
+          setPosts(response.data);
+        }
+      } catch (err) {
+        if (err.response) {
+          //not in 200 Response range
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error :${err.message}`);
+        }
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  useEffect(() => {
     const filteredResults = posts.filter(
-      post=> ((post.body).toLowerCase()).includes(search.toLowerCase())
-      ||
-      ((post.title).toLowerCase()).includes(search.toLowerCase())
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
     );
 
     setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
 
-  },[posts, search])
-
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    const id = posts.length ? Number(posts[posts.length-1].id) + 1 : 1;
-    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
-    const newPost = {id, title: postTitle, body: postBody, datetime: datetime};
+    const id = posts.length ? Number(posts[posts.length - 1].id) + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = {
+      id,
+      title: postTitle,
+      body: postBody,
+      datetime: datetime,
+    };
     const allPosts = [...posts, newPost];
     setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    navigate('/');
-  }
+    setPostTitle("");
+    setPostBody("");
+    navigate("/");
+  };
 
-
-
-  const handleDelete=(id)=>{
-
-    const postsList = posts.filter(post=> post.id !== id);
+  const handleDelete = (id) => {
+    const postsList = posts.filter((post) => post.id !== id);
     setPosts(postsList);
-    navigate('/'); //accessing browser history with react router and serving component instead of requesting anything from server
-  }
+    navigate("/"); //accessing browser history with react router and serving component instead of requesting anything from server
+  };
 
   return (
     <div className="App">
-      <Header  title="Daily Chronicles"/>
-      <Nav  search={search} setSearch={setSearch} />
+      <Header title="Daily Chronicles" />
+      <Nav search={search} setSearch={setSearch} />
       <Routes>
         <Route path="/" element={<Home posts={searchResults} />} />
 
-        <Route path="/post" element={<NewPost handleSubmit={handleSubmit} postTitle ={postTitle} setPostTitle={setPostTitle} postBody={postBody}
-        setPostBody={setPostBody} />} />
+        <Route
+          path="/post"
+          element={
+            <NewPost
+              handleSubmit={handleSubmit}
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+            />
+          }
+        />
 
-        <Route path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete}/>} />
+        <Route
+          path="/post/:id"
+          element={<PostPage posts={posts} handleDelete={handleDelete} />}
+        />
 
-        <Route path="/about" element={<About/>} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<Missing />} />
       </Routes>
       <Footer />
